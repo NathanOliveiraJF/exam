18b318a7a4e1f59865a0199e81d7b3537f3e50ff
Nathan Oliveira <nathanoliveira003jf@gmail.com>
2023-03-06 08:53:59 -0300

    feat: create exam route

---
+++ App/Controllers/ExamController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controllers;
+
+class ExamController 
+{
+  public function viewExam() 
+  {
+    require_once "./App/Views/Exam/exam.php";
+  }
+  
+}
--- routes.php
+++ routes.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Controllers\ExamController;
 use Pecee\SimpleRouter\SimpleRouter;
 use App\Controllers\LoginController;
 
 SimpleRouter::get('/', [LoginController::class,  'viewLogin']);
+SimpleRouter::get('/exam', [ExamController::class,  'viewExam']);
