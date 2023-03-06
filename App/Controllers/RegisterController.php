68c2fc7f098b8dad04af0056b70b3d84db55f841
Nathan Oliveira <nathanoliveira003jf@gmail.com>
2023-03-06 09:03:25 -0300

    feat: create register route

---
+++ App/Controllers/RegisterController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controllers;
+
+class RegisterController 
+{
+  public function viewRegister() 
+  {
+    require_once "./App/Views/Register/register.php";
+  }
+  
+}
--- routes.php
+++ routes.php
@@ -3,6 +3,8 @@
 use App\Controllers\ExamController;
 use Pecee\SimpleRouter\SimpleRouter;
 use App\Controllers\LoginController;
+use App\Controllers\RegisterController;
 
 SimpleRouter::get('/', [LoginController::class,  'viewLogin']);
 SimpleRouter::get('/exam', [ExamController::class,  'viewExam']);
+SimpleRouter::get('/register', [RegisterController::class,  'viewRegister']);
